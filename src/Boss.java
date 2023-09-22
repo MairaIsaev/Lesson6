52dc1fcd3c9cc8f996e0c461322d61740511a0ab
Mairalsaev <mairaisaev1003@gmail.com>
2023-09-22 16:52:50 +0300

    Project Finished

--- src/Boss.java
+++ src/Boss.java
@@ -3,4 +3,37 @@ public class Boss {
     int health;
     int damage;
     String protection;
+
+
+    public String getBoss() {
+        return Boss;
+    }
+
+    public void setBoss(String boss) {
+        Boss = boss;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getProtection() {
+        return protection;
+    }
+
+    public void setProtection(String protection) {
+        this.protection = protection;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,19 @@ public class Main {
         play.health = 500;
         play.damage = 100;
         play.superpower = "invisible";
-
-
+        System.out.println("Hero" + play.Naruto);
+        System.out.println("health" + play.health);
         System.out.println("damage" + play.damage);
         System.out.println("superpower" + play.superpower);
 
         Boss play2 = new Boss();
+        play2.Boss = "Boss";
         play2.health = 1000;
         play2.damage = 150;
         play2.protection = "shield";
 
 
-        play2.protection = "protection";
+        System.out.println("Boss");
         System.out.println("health" + play2.health);
         System.out.println("damage" + play2.damage);
         System.out.println("protection" + play2.protection);
